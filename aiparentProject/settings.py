d6cee125316bb2ce94e60308dc99fdd4482192e9
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-02-02 02:38:21 +0000

    Removed obsolete productionstatic dir

--- aiparentProject/settings.py
+++ aiparentProject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'productionstatic'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR]
 
